142c691936e62f22cab66b8fdea53fc07fc0cdb1
kaiqd <kaiquediniz1204@gmail.com>
2024-06-11 09:49:48 -0300

    Initial Commit

---
+++ SwiftDataTest/SwiftDataTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTestApp.swift
+//  SwiftDataTest
+//
+//  Created by Kaique Diniz on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
