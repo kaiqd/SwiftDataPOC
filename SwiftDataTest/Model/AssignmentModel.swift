a3742754ad73e46fb0df32440cf69a297dc84076
kaiqd <kaiquediniz1204@gmail.com>
2024-06-12 11:52:19 -0300

    .

---
+++ SwiftDataTest/Model/AssignmentModel.swift
@@ -0,0 +1,18 @@
+//
+//  AssignmentModel.swift
+//  SwiftDataTest
+//
+//  Created by Kaique Diniz on 11/06/24.
+//
+
+import SwiftUI
+
+struct AssignmentModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AssignmentModel()
+}
